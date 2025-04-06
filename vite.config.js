db0b4ef7a9f5487ff241ea178fcdb5a3e36fa965
Harshavardana Reddy <pattiputtoor20050320@gmail.com>
2025-04-06 13:19:20 +0530

    Vite Configuration Fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    // Enables React Router to handle routes like /skills, /about, etc.
+    historyApiFallback: true
+  }
+});
